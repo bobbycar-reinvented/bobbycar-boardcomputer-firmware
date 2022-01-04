a0254e36bfc34f6b4a2fbcef89ac34e037c05293
CommanderRedYT <ccommanderred@gmail.com>
2022-01-04 21:18:49 +0100

    Added labels for percentages

--- main/displays/menus/batterymenu.h
+++ main/displays/menus/batterymenu.h
@@ -3,6 +3,7 @@
 // local includes
 #include "displays/bobbymenudisplay.h"
 #include "widgets/doubleprogressbar.h"
+#include "widgets/label.h"
 
 class BatteryMenu : public BobbyMenuDisplay
 {
@@ -13,10 +14,13 @@ public:
 
     std::string text() const override;
 
+    void initScreen() override;
     void start() override;
     void redraw() override;
     void back() override;
 
 private:
     bobbygui::DoubleProgressBar m_doubleProgressBarBatPercentage{75, 68, 90, 24, 0, 100, TFT_RED, TFT_GREEN};
+    espgui::Label m_batPercentNowLabel {170, 67};
+    espgui::Label m_batPercentBootLabel{170, 83};
 };
