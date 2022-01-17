ffd98375ccfbf075c60c1f42daa092b72ecfe881
CommanderRedYT <ccommanderred@gmail.com>
2022-01-17 18:05:19 +0100

    Satisfying sonarcloud

--- main/bobbyschedulertask.h
+++ main/bobbyschedulertask.h
@@ -7,7 +7,7 @@ class BobbySchedulerTask : public espcpputils::SchedulerTask {
 public:
     using SchedulerTask::SchedulerTask;
     void setup() const { SchedulerTask::setup(); m_wasInitialized = true; }
-    bool isInitialized() { return m_wasInitialized; }
+    bool isInitialized() const { return m_wasInitialized; }
 private:
     mutable bool m_wasInitialized{false};
 };
