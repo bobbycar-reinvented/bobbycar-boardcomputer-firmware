c1814c0341bbf8648d0313b9fd8f61c5abe0a474
CommanderRedYT <ccommanderred@gmail.com>
2023-04-07 11:18:00 +0200

    Fixed crash

--- main/screens/speedinfodisplay.cpp
+++ main/screens/speedinfodisplay.cpp
@@ -19,8 +19,14 @@ void SpeedInfoDisplay::initScreen(espgui::TftInterface &tft)
 {
     Base::initScreen(tft);
 
-    m_dischargingBar.construct(10, PROGRESS_BAR_START_Y, tft.width()/2 - 10, 25, 0, 40, TFT_GREEN);
-    m_chargingBar.construct(tft.width()/2, PROGRESS_BAR_START_Y, tft.width()/2 - 10, 25, 0, 40, TFT_RED);
+    if (!m_dischargingBar)
+    {
+        m_dischargingBar.construct(10, PROGRESS_BAR_START_Y, tft.width() / 2 - 10, 25, 0, 40, TFT_GREEN);
+    }
+    if (!m_chargingBar)
+    {
+        m_chargingBar.construct(tft.width() / 2, PROGRESS_BAR_START_Y, tft.width() / 2 - 10, 25, 0, 40, TFT_RED);
+    }
 
     m_labelSpeed.start(tft);
 
@@ -41,25 +47,34 @@ void SpeedInfoDisplay::redraw(espgui::TftInterface &tft)
 
     Base::redraw(tft);
 
-    m_dischargingBar->setMinMax(0, profileSettings.limits.iDcMax * controllers.size() * 2);
-    m_chargingBar->setMinMax(0, profileSettings.limits.iDcMax * controllers.size() * 2);
+    if (m_dischargingBar)
+    {
+        m_dischargingBar->setMinMax(0, profileSettings.limits.iDcMax * controllers.size() * 2);
+    }
+    if (m_chargingBar)
+    {
+        m_chargingBar->setMinMax(0, profileSettings.limits.iDcMax * controllers.size() * 2);
+    }
 
     auto font = espgui::FontRenderer{tft};
 
     font.setTextSize(4);
 
     m_labelSpeed.redraw(tft, font,
-            std::abs(avgSpeedKmh) < 10 ? fmt::format("{:.2f}", avgSpeedKmh) :
-            (std::abs(avgSpeedKmh) < 100 ? fmt::format("{:.1f}", avgSpeedKmh) : fmt::format("{:.0f}", avgSpeedKmh)), espgui::TFT_WHITE, espgui::TFT_BLACK, 4);
+                        std::abs(avgSpeedKmh) < 10 ? fmt::format("{:.2f}", avgSpeedKmh) :
+                        (std::abs(avgSpeedKmh) < 100 ? fmt::format("{:.1f}", avgSpeedKmh) :
+                         fmt::format("{:.0f}", avgSpeedKmh)), espgui::TFT_WHITE, espgui::TFT_BLACK, 4);
 
     font.setTextSize(1);
-    m_batteryPercentLabel.redraw(tft, font, battery::getBatteryPercentageString(), espgui::TFT_WHITE, espgui::TFT_BLACK, 4);
+    m_batteryPercentLabel.redraw(tft, font, battery::getBatteryPercentageString(), espgui::TFT_WHITE, espgui::TFT_BLACK,
+                                 4);
 
     if (const auto avgVoltage = controllers.getAvgVoltage(); avgVoltage)
     {
         auto watt = sumCurrent * *avgVoltage;
 
-        m_voltageLabel.redraw(tft, font, fmt::format("{:.1f} V", avgVoltage.value()), espgui::TFT_WHITE, espgui::TFT_BLACK, 4);
+        m_voltageLabel.redraw(tft, font, fmt::format("{:.1f} V", avgVoltage.value()), espgui::TFT_WHITE,
+                              espgui::TFT_BLACK, 4);
         font.setTextSize(2);
         m_currentPowerLabel.redraw(tft, font, fmt::format("{:.0f} W", watt), espgui::TFT_WHITE, espgui::TFT_BLACK, 4);
         font.setTextSize(1);
@@ -71,15 +86,21 @@ void SpeedInfoDisplay::redraw(espgui::TftInterface &tft)
     }
 
     m_distanceLabel.redraw(tft, font,
-            drivingStatistics.meters_driven > 1000 ? fmt::format("{:.3f} km", drivingStatistics.meters_driven / 1000) :
-            (drivingStatistics.meters_driven > 100 ? fmt::format("{:.1f} m", drivingStatistics.meters_driven) : fmt::format("{:.2f} m", drivingStatistics.meters_driven)), espgui::TFT_WHITE, espgui::TFT_BLACK, 4);
+                           drivingStatistics.meters_driven > 1000 ?
+                           fmt::format("{:.3f} km", drivingStatistics.meters_driven / 1000) :
+                           (drivingStatistics.meters_driven > 100 ?
+                            fmt::format("{:.1f} m", drivingStatistics.meters_driven) :
+                            fmt::format("{:.2f} m", drivingStatistics.meters_driven)), espgui::TFT_WHITE,
+                           espgui::TFT_BLACK, 4);
 
     font.setTextSize(1);
 
     m_dischargingBar->redraw(tft, sumCurrent < 0.f ? (-sumCurrent) : 0.f);
     m_chargingBar->redraw(tft, sumCurrent > 0.f ? sumCurrent : 0.f);
 
-    m_performanceLabel.redraw(tft, std::to_string(drivingModeTask.callCount()), (drivingModeTask.callCount() < 35) ? espgui::TFT_ORANGE : espgui::TFT_WHITE, espgui::TFT_BLACK, 2);
+    m_performanceLabel.redraw(tft, std::to_string(drivingModeTask.callCount()),
+                              (drivingModeTask.callCount() < 35) ? espgui::TFT_ORANGE : espgui::TFT_WHITE,
+                              espgui::TFT_BLACK, 2);
 }
 
 void SpeedInfoDisplay::buttonPressed(espgui::Button button)
@@ -88,7 +109,7 @@ void SpeedInfoDisplay::buttonPressed(espgui::Button button)
 
     switch (button)
     {
-    using espgui::Button;
+        using espgui::Button;
     case Button::Right:
         espgui::pushScreen<MainMenu>();
         break;
